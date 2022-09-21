0a51a39da457db0532e6b6bf7ed12ced6c444824
Rajit Ahmed <overrated270@gmail.com>
2022-09-21 16:07:28 -0400

    Homework Completed!!! Yay!!

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.print("New statement to see if it committed or not.");
+        System.out.println();
+        System.out.println("I'm writing this and committing this at home!");
+        System.out.print("Hi future me at school tomorrow!");
     }
 }
